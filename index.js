badfb2907e0c9d70fe64178ee1f59e6a6fa13f1f
Utkarsh Kashyap <116485473+yukay2907@users.noreply.github.com>
2026-06-10 13:17:05 +0530

    added Sign-Up and Sign-In page

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const body = document.querySelector("body");
+const signInSignUpButton = document.querySelector(".sign-in-sign-up");
 
 window.addEventListener("load", () => {
   body.classList.add("visible");
+});
+
+signInSignUpButton.addEventListener("click", () => {
+  location.href = "/pages/signInsignUp/authenticate.html";
 });
